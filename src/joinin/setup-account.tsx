ee1cf5629e70ed961d128811bc79c9e86060f6a0
James <jamestoone@gmail.com>
2023-03-16 10:47:49 +0000

    separate out component

--- src/joinin/setup-account.tsx
+++ src/joinin/setup-account.tsx
@@ -9,6 +9,7 @@ import {
   Modal,
   Switch,
 } from "antd";
+import PublicMarketingColumn from "../components/publicMarketingColumn";
 const { Text, Title } = Typography;
 const { Header } = Layout;
 
@@ -200,27 +201,7 @@ function SetupAccount(): ReactElement {
         </h1>
       </Header>
       <Layout className="flex-row">
-        <div className="sticky top-0 flex-1 hidden max-h-screen lg:block">
-          <img
-            className="absolute inset-0 object-cover w-full h-full"
-            src="https://app.joinin.online/images/about-hero.jpeg"
-            alt="Background image showing a club photo in a gym"
-          />
-          <div className="absolute inset-0 grid w-full h-full p-8 bg-primary-500/70 place-items-center">
-            <div className="relative w-full mx-auto leading-normal text-white max-w-prose">
-              <div className="flex items-center mb-6 text-3xl tracking-tight">
-                <Title level={2} className="my-0 text-white">
-                  Ready to take part?
-                </Title>
-              </div>
-              <Title level={4} className="my-0 text-primary-50">
-                You're here because your club or organisation uses joinin's
-                software. Sign in to your account to register for classes, renew
-                memberships, buy merchandise, and so much more!
-              </Title>
-            </div>
-          </div>
-        </div>
+        <PublicMarketingColumn />
         <div className="flex-1 bg-white p-12 sm:p-14 sm:pt-[10vh]">
           <div className="w-full max-w-xs mx-auto space-y-6">
             <div>

288a95d9a8e9d4da269c37d5cbf8a32be3ee8a4f
James <jamestoone@gmail.com>
2023-06-20 09:44:58 +0100

    Update dev-programme.tsx

--- src/loveadmin/dev-programme/dev-programme.tsx
+++ src/loveadmin/dev-programme/dev-programme.tsx
@@ -3,10 +3,11 @@ import {
   CheckCircleFilled,
   CloseCircleFilled,
   DownloadOutlined,
+  EllipsisOutlined,
   PlayCircleFilled,
   StarFilled,
 } from "@ant-design/icons";
-import { Layout, Typography, Button, Table, Select } from "antd";
+import { Layout, Typography, Button, Table, Select, Progress } from "antd";
 import type { TableColumnsType } from "antd";
 import type { TableRowSelection } from "antd/es/table/interface";
 const { Content } = Layout;
@@ -16,8 +17,9 @@ const { Option } = Select;
 interface DataType {
   key: React.Key;
   skill: string;
-  level: string;
-  progress: string;
+  levelValue: number;
+  participants: number;
+  progress: number;
 }
 
 interface ExpandedDataType {
@@ -25,11 +27,6 @@ interface ExpandedDataType {
   name: string;
   progress: string;
 }
-
-const items = [
-  { key: "1", label: "Action 1" },
-  { key: "2", label: "Action 2" },
-];
 
 const DevProgrammeModal: React.FC = () => {
   const expandedRowRender = () => {
@@ -43,30 +40,154 @@ const DevProgrammeModal: React.FC = () => {
     for (let i = 0; i < 3; ++i) {
       data.push({
         key: i.toString(),
-        date: "2014-12-24 23:12:00",
         name: "This is production name",
-        upgradeNum: "Upgraded: 56",
+        progress: "Upgraded: 56",
       });
     }
+
     return <Table columns={columns} dataSource={data} pagination={false} />;
   };
 
   const columns: TableColumnsType<DataType> = [
-    { title: "Skill", dataIndex: "skill", key: "skill" },
-    { title: "Level", dataIndex: "level", key: "level" },
-    { title: "Progress", dataIndex: "progress", key: "progress" },
-    { title: "Action", key: "operation", render: () => <a>Publish</a> },
+    {
+      title: "Skill",
+      dataIndex: "skill",
+      key: "skill",
+      render: (text: string) => <a>{text}</a>,
+      width: 740,
+    },
+    {
+      title: "Level",
+      dataIndex: "levelValue",
+      key: "level",
+      width: 340,
+      sorter: (a, b) => a.levelValue - b.levelValue,
+      defaultSortOrder: "ascend",
+      render: (levelValue: number, record: DataType) => (
+        <div>
+          <div>Level {levelValue}</div>
+          <div className="text-neutral-500">
+            {record.participants} participants
+          </div>
+        </div>
+      ),
+    },
+    {
+      title: "Progress",
+      dataIndex: "progress",
+      key: "progress",
+      width: 340,
+      render: (progress: number, record: DataType) => (
+        <div className="pr-10">
+          <Progress
+            percent={record.progress}
+            size="small"
+            className="[&_.ant-progress-bg]:bg-primary-500"
+          />
+        </div>
+      ),
+    },
+    {
+      title: "",
+      align: "right",
+      key: "operation",
+      render: () => (
+        <Button type="text" size="small" icon={<EllipsisOutlined />}></Button>
+      ),
+    },
   ];
 
-  const data: DataType[] = [];
-  for (let i = 0; i < 3; ++i) {
-    data.push({
-      key: i.toString(),
-      skill: "Screen",
-      level: "iOS",
-      progress: "10.3.4.5654",
-    });
-  }
+  const data = [
+    {
+      key: "1",
+      skill: "Enter the water safely",
+      levelValue: 1,
+      participants: 7,
+      progress: 70,
+    },
+    {
+      key: "2",
+      skill:
+        "Move forward for a distance of 5 metres, feet may be on or off the floor",
+      levelValue: 1,
+      participants: 7,
+      progress: 60,
+    },
+    {
+      key: "3",
+      skill:
+        "Move backwards for a distance of 5 metres, feet may be on or off the floor",
+      levelValue: 1,
+      participants: 7,
+      progress: 30,
+    },
+    {
+      key: "4",
+      skill:
+        "Move sideways for a distance of 5 metres, feet may be on or off the floor",
+      levelValue: 1,
+      participants: 7,
+      progress: 80,
+    },
+    {
+      key: "5",
+      skill: "Scoop the water and wash the face",
+      levelValue: 1,
+      participants: 7,
+      progress: 20,
+    },
+    {
+      key: "6",
+      skill: "Be comfortable with water showered from overhead",
+      levelValue: 1,
+      participants: 7,
+      progress: 10,
+    },
+    {
+      key: "7",
+      skill:
+        "Move from a flat floating position on the back and return to standing",
+      levelValue: 1,
+      participants: 7,
+      progress: 40,
+    },
+    {
+      key: "8",
+      skill:
+        "Move from a flat floating position on the front and return to standing",
+      levelValue: 1,
+      participants: 7,
+      progress: 40,
+    },
+    {
+      key: "9",
+      skill: "Push and glide in a flat position on the front from a wall",
+      levelValue: 2,
+      participants: 14,
+      progress: 10,
+    },
+    {
+      key: "10",
+      skill: "Push and glide in a flat position on the back from a wall",
+      levelValue: 2,
+      participants: 14,
+      progress: 40,
+    },
+    {
+      key: "11",
+      skill: "Give examples of two pool rules",
+      levelValue: 2,
+      participants: 14,
+      progress: 40,
+    },
+    {
+      key: "12",
+      skill: "Exit the water safely",
+      levelValue: 2,
+      participants: 14,
+      progress: 40,
+    },
+  ];
 
   const rowSelection: TableRowSelection<DataType> = {
     onChange: (selectedRowKeys, selectedRows) => {
@@ -184,6 +305,7 @@ const DevProgrammeModal: React.FC = () => {
               size="small"
               pagination={false}
               rowSelection={{ ...rowSelection }}
+              className="ant-table-sticky"
             />
           </div>
           <footer className="fixed gap-2 bottom-0 flex items-center transition-all right-0 z-30 py-2.5 px-4 bg-white border-t border-b-0 border-solid border-x-0 border-neutral-200">

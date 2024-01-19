143fcbbb4631f2269338cc2ad34758096fe7ce20
James <jamestoone@gmail.com>
2024-01-19 17:39:14 +0000

    Update form-builder.tsx

--- src/loveadmin/settings/data/form-builder/form-builder.tsx
+++ src/loveadmin/settings/data/form-builder/form-builder.tsx
@@ -753,18 +753,35 @@ const FormBuilder = () => {
               </div>
             </Form.Item>
           )}
-
-          <Form.Item
-            label="Label"
-            name="label"
-            rules={[{ required: true, message: "Please enter a field label" }]}
-          >
-            <Input />
-          </Form.Item>
-
           {editFieldId && (
-            <Form.Item label="Type">
-              <div>
+            <Form.Item label="Type" className="-mt-1">
+              <div className="flex items-center gap-2">
+                {formFields.find((field) => field.id === editFieldId)
+                  ?.inputType === "Dropdown" && (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    className="relative w-4 h-4 left-px text-neutral-400"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5.22 10.22a.75.75 0 0 1 1.06 0L8 11.94l1.72-1.72a.75.75 0 1 1 1.06 1.06l-2.25 2.25a.75.75 0 0 1-1.06 0l-2.25-2.25a.75.75 0 0 1 0-1.06ZM10.78 5.78a.75.75 0 0 1-1.06 0L8 4.06 6.28 5.78a.75.75 0 0 1-1.06-1.06l2.25-2.25a.75.75 0 0 1 1.06 0l2.25 2.25a.75.75 0 0 1 0 1.06Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                )}
+                {formFields.find((field) => field.id === editFieldId)
+                  ?.inputType === "Radio" && (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    className="w-4 h-4 text-neutral-400"
+                  >
+                    <path d="M3 4.75a1 1 0 1 0 0-2 1 1 0 0 0 0 2ZM6.25 3a.75.75 0 0 0 0 1.5h7a.75.75 0 0 0 0-1.5h-7ZM6.25 7.25a.75.75 0 0 0 0 1.5h7a.75.75 0 0 0 0-1.5h-7ZM6.25 11.5a.75.75 0 0 0 0 1.5h7a.75.75 0 0 0 0-1.5h-7ZM4 12.25a1 1 0 1 1-2 0 1 1 0 0 1 2 0ZM3 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z" />
+                  </svg>
+                )}
                 {
                   formFields.find((field) => field.id === editFieldId)
                     ?.inputType
@@ -772,6 +789,13 @@ const FormBuilder = () => {
               </div>
             </Form.Item>
           )}
+          <Form.Item
+            label="Label"
+            name="label"
+            rules={[{ required: true, message: "Please enter a field label" }]}
+          >
+            <Input />
+          </Form.Item>
 
           <Form.Item label="Options">
             <DragDropContext
